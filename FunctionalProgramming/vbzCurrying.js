9e4189dc66c2a99e30f6091c5090755e509e7140
vbitzceo <vbitzceo@gmail.com>
2020-12-06 08:01:12 -0500

    a bit about closure in currying

--- FunctionalProgramming/vbzCurrying.js
+++ FunctionalProgramming/vbzCurrying.js
@@ -1,6 +1,16 @@
 // Currying allows us to partially specify parameters
 let buildPerson = firstName => lastName => Object.assign({},{ firstName: firstName, lastName: lastName});
 
+//the above could be written as follows:
+// let buildPerson = function(firstName) {
+//     return function(lastName) {
+//         //NOTE: this function creates a closure which holds on to
+//         //the properties and their values in the LEXICAL environment at this time
+//         //this is why partial's work
+//         return Object.assign({},{ firstName: firstName, lastName: lastName});
+//     }
+// }
+
 // lets use this to make several people with the same first name
 let partialPerson = buildPerson("John");
 
